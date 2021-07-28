f678eb4cee3e80ecd015a3eb1aa1126021ae274a
Troy Dorman <troydorman@gmail.com>
2021-07-28 07:44:11 -0600

    fixed bootstrap link. made axios call to api and received text data

--- src/App.js
+++ src/App.js
@@ -13,6 +13,28 @@ class App extends Component {
     };
   }
 
+  componentWillMount() {
+    this.getSampleText();
+  }
+
+  getSampleText() {
+    axios
+      .get(
+        "http://hipsterjesus.com/api?paras=" +
+          this.state.paras +
+          "&html=" +
+          this.state.html
+      )
+      .then((response) => {
+        this.setState({ text: response.data.text }, function () {
+          console.log(this.state);
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return <div className="App">Hello</div>;
   }
